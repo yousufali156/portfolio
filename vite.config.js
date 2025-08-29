327eb26a727c1495f825df3b0bc18c9f99810ac2
yousufali156 <yousufwebwork@gmail.com>
2025-08-30 01:25:04 +0600

    Version 1.2.0

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: '.',   
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  }
+})
